1810d1e98f7f86ea90a2e5514776dc00a16f589a
Jesper Fritsch <jesperf96@gmail.com>
2024-01-13 17:24:34 +0100

    updating scripts

--- driver/driver.py
+++ driver/driver.py
@@ -271,7 +271,7 @@ async def handleNewImage(new_image_queue):
 def newImageEvent(loop, new_image_queue, event):
     if isinstance(event.data, str):
         img_path = event.data
-        asyncio.run_coroutine_threadsafe(new_image_queue.put(img_path), loop)
+        new_image_queue.put(img_path)
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
