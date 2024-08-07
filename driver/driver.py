494fe3c5aa041b190347de19c0da82f3cdfaa822
Jesper Fritsch <jesperf96@gmail.com>
2024-08-07 21:21:21 +0200

    Adjusted sleep for socket recieve

--- driver/driver.py
+++ driver/driver.py
@@ -170,7 +170,7 @@ class SnakeHandler:
                 except Exception as e:
                     print(e)
                     break
-                await asyncio.sleep(1 / (self.fps * 3))
+                await asyncio.sleep(1 / (self.fps * 2))
         except Exception as e:
             print(e)
             self.running = False
